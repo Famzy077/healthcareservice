96adeaa93bb7ca5ad5785b50d42d00b4341fa17c
Akinola Femi <149377080+Famzy077@users.noreply.github.com>
2024-11-05 21:34:39 +0100

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -32,12 +32,12 @@ const corsOptions = {
 
 
 
-// app.use((req, res, next) => {
-//      res.setHeader('Access-Control-Allow-Origin', 'https://healthcareservice-client.vercel.app');
-//      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//      res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//      next();
-//    });
+app.use((req, res, next) => {
+     res.setHeader('Access-Control-Allow-Origin', 'https://healthcareservice-client.vercel.app');
+     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+     next();
+   });
 
 // Connected To DataBase
 connectDb()
