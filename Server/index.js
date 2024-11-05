0b7482e5bd4beb4a8e5ade3c8af33fea845afdb5
Akinola Femi <149377080+Famzy077@users.noreply.github.com>
2024-11-05 21:22:38 +0100

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -8,28 +8,41 @@ import cookieParser from 'cookie-parser'
 dotenv.config()
 const PORT = process.env.PORT || 5000
 const app = express()
-// let whiteList = ['https://vercel.com']
-// const corsOptions = {
-//     origin: function(origin, callback) {
-//         if(whiteList.indexOf(origin) !== -1) {
-//             callback(null, true)
-//         } else {
-//             callback(new Error('Block by CORS'))
-//         }
-//     },
-//     optionSuccessStatus: 200
-// }
-app.use((req, res, next) => {
-     res.setHeader('Access-Control-Allow-Origin', 'https://healthcareservice-client.vercel.app');
-     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-     next();
-   });
+
+const cors = require('cors');
+const express = require('express');
+const app = express();
+
+const allowedOrigins = [
+  'http://localhost:5173', // Local dev
+  'https://healthcareservice-client-pl6pbjrgt-akinola-femis-projects.vercel.app' // Vercel production
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'OPTIONS'],
+  credentials: true
+};
+
+app.use(cors(corsOptions));
+
+
+// app.use((req, res, next) => {
+//      res.setHeader('Access-Control-Allow-Origin', 'https://healthcareservice-client.vercel.app');
+//      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//      res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//      next();
+//    });
 
 // Connected To DataBase
 connectDb()
 app.use(express.json())
-app.use(cors())
 app.use(cookieParser())
 app.use('/auth', router);
 app.listen(PORT, () => {
