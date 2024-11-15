020838955d12ed721b92aca4457000e22c742bcf
Akinola Femi <149377080+Famzy077@users.noreply.github.com>
2024-11-15 21:25:11 +0100

    Update DbConnect.js

--- Server/config/DbConnect.js
+++ Server/config/DbConnect.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 const connectDb = async() => {
     try {
-        // const conn = await mongoose.connect(process.env.MONGO_KEY)
-        const conn = await mongoose.connect('mongodb://localhost:27017/auth')
+        const conn = await mongoose.connect(process.env.MONGO_KEY)
+        // const conn = await mongoose.connect('mongodb://localhost:27017/auth')
         console.log(`Connected to the data base successfully ${conn.connection.host}`)
     } catch (error) {
         console.log(error, 'Mongodb is not connect to database');
